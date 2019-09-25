ffd15d77f6d77b1420be7d586ab1c07c92be6a8b
Alf Urrego <ingalfurrego@gmail.com>
2019-09-25 00:09:55 -0500

    ajuste ruta imagenes

--- wp-content/themes/understrap/footer.php
+++ wp-content/themes/understrap/footer.php
@@ -37,9 +37,9 @@ $container = get_theme_mod( 'understrap_container_type' );
 				<div class="col-md-4 div-item-der-footer">
 					<div class="row">
 		        <div class="col-6 div-item-rs">
-		         <a class="fb-ic mr-3" role="button" href="https://www.instagram.com/esoes_design/" target="_blank"><img src="./wp-content/uploads/2019/08/Instagram.png" alt=""></a>
-		         <a class="fb-fb mr-3" role="button" href="https://www.facebook.com/esoes.design" target="_blank"><img src="./wp-content/uploads/2019/08/facebook.png" alt=""></a>
-		         <a class="fb-ic mr-3" role="button" href="https://www.pinterest.ch/esoes_design/" target="_blank"><img src="./wp-content/uploads/2019/08/Pinteres.png" alt=""></a>
+		         <a class="fb-ic mr-3" role="button" href="https://www.instagram.com/esoes_design/" target="_blank"><img src="/wp-content/uploads/2019/08/Instagram.png" alt=""></a>
+		         <a class="fb-fb mr-3" role="button" href="https://www.facebook.com/esoes.design" target="_blank"><img src="/wp-content/uploads/2019/08/facebook.png" alt=""></a>
+		         <a class="fb-ic mr-3" role="button" href="https://www.pinterest.ch/esoes_design/" target="_blank"><img src="/wp-content/uploads/2019/08/Pinteres.png" alt=""></a>
 		        </div>
 		        <div class="col-6 div-item-copy ">
 		          <p><span>&#169;</span>&nbsp;&nbsp;  C O P Y R I G H T 2 0 1 9 | E S O E S </p>
@@ -93,7 +93,7 @@ $container = get_theme_mod( 'understrap_container_type' );
     e.preventDefault();
     $('html, body').animate({scrollTop:0}, '100');
   });
-	$('.search-icon').replaceWith("<img class='img-lupe' src='./wp-content/uploads/2019/08/loupe.png'>");
+	$('.search-icon').replaceWith("<img class='img-lupe' src='/wp-content/uploads/2019/08/loupe.png'>");
 
 })( jQuery );
 
